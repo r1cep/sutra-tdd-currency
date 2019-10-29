4b729683d81501d8fafec873fa59497403aeb1db
r1cep <dev.r1cep@gmail.com>
2019-10-29 23:15:14 +0900

    :sparkles: 第11章 不要になったら消す The Root of All Evil.

--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use App\Franc;
 use App\Money;
 use PHPUnit\Framework\TestCase;
 
@@ -21,27 +20,12 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
         $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
-        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
-        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
         $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
-    }
-
-    public function testFrancManipulation(): void
-    {
-        $five = Money::franc(5);
-        // Javaと違ってequalsメソッドのオーバーライドがないのでTDD本と違う
-        $this->assertTrue(Money::franc(10)->equals($five->times(2)));
-        $this->assertTrue(Money::franc(15)->equals($five->times(3)));
     }
 
     public function testCurrency(): void
     {
         $this->assertEquals('USD', Money::dollar(1)->currency());
         $this->assertEquals('CHF', Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue((new Money(10, 'CHF'))->equals(new Franc(10, 'CHF')));
     }
 }
